a427981499e07cfe8ba9cc25232cf9667eed4a10
Ana Polese <34096402+anapolese@users.noreply.github.com>
2020-12-07 10:43:00 -0300

    code refactoring

--- numExtenso/js/numbers.js
+++ numExtenso/js/numbers.js
@@ -1,49 +1,45 @@
-var centena        = ['Cento', 'Cem', 'Duzentos', 'Trezentos', 'Quatrocentos', 'Quinhentos', 'Seiscentos', 'Setecentos', 'Oitocentos', 'Novecentos'],
-    dezena         = ['Vinte', 'Trinta', 'Quarenta', 'Cinquenta', 'Sessenta', 'Setenta', 'Oitenta', 'Noventa'],
-    numEspeciais   = ['Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove'],
-    unidade        = ['Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove'],
-    slider         = document.querySelector('#myRange'),
-    numAtual       = document.querySelector('#number'),
-    writtenNum     = document.querySelector('#writtenNumber');
+var hundreds    = ['Cento', 'Duzentos', 'Trezentos', 'Quatrocentos', 'Quinhentos', 'Seiscentos', 'Setecentos', 'Oitocentos', 'Novecentos'],
+    tens        = ['Vinte', 'Trinta', 'Quarenta', 'Cinquenta', 'Sessenta', 'Setenta', 'Oitenta', 'Noventa'], 
+    exceptions  = {0: 'Zero', 100: 'Cem'},
+    tensExcep   = ['Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove'],
+    units       = ['', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove'],
+    slider      = document.querySelector('#myRange'),
+    number      = document.querySelector('#number'),
+    writtenNum  = document.querySelector('#writtenNumber');
 
 function updateValue() {
-    numAtual.value = slider.value;
-    testNumber(numAtual.value);
+    number.value = slider.value;
+    writtenNum.value = testNumber(number.value);
 }
 
 
 function testNumber(a) {
-    if (a.length === 1) unidades(a);
-    else if (a.length === 2) dezenas(a);
-    else centenas(a);
+    if (exceptions[a]) return exceptions[a];
+    else if (a.length === 1) return unit(a);
+    else if (a.length === 2) return dezenas(a);
+    else if (a.length === 3) return hundred(a);
+    else return 'Ops... Eu ainda não aprendi esse número'
 }
 
-function unidades(a) {
-    writtenNum.value = unidade[parseInt(a)];
+function unit(a) {
+   return units[a];
 }
 
-function dezenas(a) {
-    if (a[0] === '1') writtenNum.value = numEspeciais[parseInt(a[1])]; 
-    else if ((a[0] !== '1') && (a[1] === '0')) writtenNum.value = dezena[parseInt(a[0]-2)];
-    else writtenNum.value = dezena[parseInt(a[0]-2)] + ' e ' + unidade[parseInt(a[1])];
+function ten(a) {
+    if (a[0] === '1') return tensExcep[parseInt(a[1])]; 
+    else if ((a[0] !== '1') && (a[1] === '0')) return tens[parseInt(a[0]-2)];
+    else return tens[parseInt(a[0]-2)] + ' e ' + units[parseInt(a[1])];
 }
 
-function centenas(a) {
+function hundred(a) {
     //centenas INTEIRAS
-    if ((a[0] !== '0') && (a[1] === '0') && (a[2] === '0')) writtenNum.value = centena[parseInt(a[0])]; 
-    // cento + unidades
-    else if ((a[0] === '1') && (a[1] === '0') && (a[2] !== '0')) writtenNum.value = centena[0] + ' e ' + unidade[parseInt(a[2])];
-    //cento + carac especiais
-    else if ((a[0] === '1') && (a[1] === '1')) writtenNum.value = centena[0] + ' e ' + numEspeciais[parseInt(a[2])];
-    //cento + dezenas
-    else if ((a[0] === '1') && (a[1] !== '1') && (a[1] !=='0') && (a[2] === '0')) writtenNum.value = centena[0] + ' e ' + dezena[parseInt(a[1]-2)];
-    //cento + dezenas compostas
-    else if ((a[0] === '1') && (a[1] !== '1') && (a[1] !=='0')  && (a[2] !== '0')) writtenNum.value = centena[0] + ' e ' + dezena[parseInt(a[1]-2)] + ' e ' + unidade[parseInt(a[2])];
-    //centenas + carac especiais
-    else if ((a[0] !== '1') && (a[1] === '1')) writtenNum.value = centena[parseInt(a[0])] + ' e ' + numEspeciais[parseInt(a[2])];
-    //centenas + unidades
-    else if ((a[0] !== '1') && (a[1] === '0') && (a[2] !== '0')) writtenNum.value = centena[parseInt(a[0])] + ' e ' + unidade[parseInt(a[2])];
-    //centenas COMPOSTAS
-    else if ((a[0] !== '1') && (a[1] !== '1') && (a[2] !== '0')) writtenNum.value = centena[parseInt(a[0])] + ' e ' + dezena[parseInt(a[1]-2)]+ ' e ' + unidade[parseInt(a[2])];
-    else writtenNum.value = centena[parseInt(a[0])] + ' e ' + dezena[parseInt(a[1]-2)];
+    if ((a[1] === '0') && (a[2] === '0')) return hundreds[parseInt(a[0]-1)]; 
+    // centenas + unidades
+    else if ((a[1] === '0') && (a[2] !== '0')) return hundreds[parseInt(a[0]-1)]+ ' e ' + units[parseInt(a[2])];
+    // centenas + carac especiais
+    else if ((a[1] === '1')) return hundreds[parseInt(a[0]-1)] + ' e ' + tensExcep[parseInt(a[2])];
+    //centenas + dezenas
+    else if ((a[1] !=='0') && (a[2] === '0')) return hundreds[parseInt(a[0]-1)] + ' e ' + tens[parseInt(a[1]-2)];
+    //centenas + dezenas compostas
+    else return hundreds[parseInt(a[0]-1)] + ' e ' + tens[parseInt(a[1]-2)] + ' e ' + units[parseInt(a[2])];
 }
